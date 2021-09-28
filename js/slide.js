335e0a5b230576f6ee80283bafe499986de10ed8
Douglas Borges <douglasborges19@gmail.com>
2021-09-28 15:53:37 -0300

    adiciona atualização da posição do slide durante os eventos

--- js/slide.js
+++ js/slide.js
@@ -2,16 +2,37 @@ export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
+        this.dist = {
+            startX: 0,
+            movement: 0,
+            movePosition: 0,
+            finalPositon: 0,
+        };
+    }
+
+    updatePosition(clienteX) {
+        this.dist.movement = this.dist.startX - clienteX;
+        return this.dist.finalPositon - this.dist.movement;
+    }
+
+    moveSlide(distX) {
+        this.dist.movePosition = distX;
+        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
     }
 
     onStart(event) {
         event.preventDefault();
+        this.dist.startX = event.clientX;
         this.wrapper.addEventListener('mousemove', this.onMove);
     }
 
-    onMove(event) {}
+    onMove(event) {
+        const endPosition = this.updatePosition(event.clientX);
+        this.moveSlide(endPosition);
+    }
 
     onEnd(event) {
+        this.dist.finalPositon = this.dist.movePosition;
         this.wrapper.removeEventListener('mousemove', this.onMove);
     }
 
